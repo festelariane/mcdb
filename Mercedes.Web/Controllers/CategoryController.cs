7c0f5ea90153051ef14edce5294ec0af3fe953a1
thanhhale <lethanhhadev@gmail.com>
2017-07-06 15:59:29 +0700

    add category

---
+++ Mercedes.Web/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Mercedes.Services.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mercedes.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICarService _carService;
+        public CategoryController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Category
+        [HttpGet]
+        public ActionResult Category()
+        {
+            var allCategory = _carService.GetAllCategory();
+            return PartialView("Category", allCategory);
+        }
+    }
+}
