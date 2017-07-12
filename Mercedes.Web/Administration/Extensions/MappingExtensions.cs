a27cc8e9105c7fc4c9996350efebf737bed8b3a5
festelariane <trinhthaituyen2003@yahoo.com>
2017-07-12 09:02:48 +0700

    Add missing files

---
+++ Mercedes.Web/Administration/Extensions/MappingExtensions.cs
@@ -0,0 +1,27 @@
+﻿using FastMapper;
+using Mercedes.Admin.Models;
+using Mercedes.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mercedes.Admin.Extensions
+{
+    public static class MappingExtensions
+    {
+        public static CategoryModel ToModel(this Category entity)
+        {
+            return TypeAdapter.Adapt<Category, CategoryModel>(entity);
+        }
+        public static Category ToEntity(this CategoryModel model)
+        {
+            return TypeAdapter.Adapt<CategoryModel, Category>(model);
+        }
+
+        public static Category ToEntity(this CategoryModel model, Category destination)
+        {
+            return TypeAdapter.Adapt(model,destination);
+        }
+    }
+}
