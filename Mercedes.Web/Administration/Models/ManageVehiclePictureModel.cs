14c26635edc3567e3ba16742e42bc9b11bac09d7
festelariane <trinhthaituyen2003@yahoo.com>
2017-07-13 22:26:15 +0700

    Add missing files

---
+++ Mercedes.Web/Administration/Models/ManageVehiclePictureModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mercedes.Admin.Models
+{
+    public class ManageVehiclePictureModel
+    {
+        public ManageVehiclePictureModel()
+        {
+            VehiclePictures = new List<VehiclePictureModel>();
+            NewPictureModel = new VehiclePictureModel();
+        }
+        public int VehicleModelId { get; set; }
+        public List<VehiclePictureModel> VehiclePictures { get; set; }
+        public VehiclePictureModel NewPictureModel { get; set; }
+    }
+}
